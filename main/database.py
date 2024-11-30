472a945dfb11f5249874580d0983a597d7d8b264
qrob <abdraximm@gmail.com>
2024-11-30 10:32:47 +0500

    login fix nakonec

--- main/database.py
+++ main/database.py
@@ -5,12 +5,9 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
